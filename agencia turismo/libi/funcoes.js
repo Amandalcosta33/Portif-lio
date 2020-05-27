7166ad69ad7b2bbbab202df47a37b29519d7eb71
Amanda.Lopes <amanda.lcosta33@gmail.com>
2020-05-26 22:07:22 -0300

    feito agencia de turismo com materialize

---
+++ agencia turismo/libi/funcoes.js
@@ -0,0 +1,15 @@
+$('.sidenav').sidenav();
+
+$('.carousel.carousel-slider').carousel({
+    indicators : true,
+    fullWidth : true
+});
+
+autoplay();
+
+function autoplay(){
+    $('.carousel.carousel-slider').carousel('next');
+    setTimeout(autoplay, 5000);
+}
+
+$('.modal').modal();
